4708990044be0167444c5c3d9916976e01c27f6c
sarveshkarki <sarveshkarki93@gmail.com>
2026-01-22 22:54:48 +0545

    postgres integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,28 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        port: 5432,
+        username: 'postgres',
+        password: '1234',
+        host: 'localhost',
+        database: 'nestjs-blog',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
