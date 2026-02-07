6f83493d5eaec0d486a14772e730a11aa0209a81
sarveshkarki <sarveshkarki93@gmail.com>
2026-02-07 14:28:10 +0545

    soft delete

--- src/tags/providers/tags.service.ts
+++ src/tags/providers/tags.service.ts
@@ -25,4 +25,20 @@ export class TagsService {
     });
     return results;
   }
+
+  public async delete(id: number) {
+    await this.tagsRepository.delete(id);
+    return {
+      deleted: true,
+      id,
+    };
+  }
+
+  public async softRemove(id: number) {
+    await this.tagsRepository.softDelete(id);
+    return {
+      deleted: true,
+      id,
+    };
+  }
 }
--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateTagDto } from './dtos/create-tag.dto';
 import { TagsService } from './providers/tags.service';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
@@ -12,5 +19,17 @@ export class TagsController {
   @Post()
   public create(@Body() createTagDto: CreateTagDto) {
     return this.tagsService.create(createTagDto);
+  }
+
+  @ApiOperation({ summary: 'Delete a  tag' })
+  @ApiResponse({ status: 201, description: 'The tag has been deleted.' })
+  @Delete()
+  public async delete(@Query('id', ParseIntPipe) id: number) {
+    return this.tagsService.delete(id);
+  }
+
+  @Delete('soft-delete')
+  public async softDelete(@Query('id', ParseIntPipe) id: number) {
+    return this.tagsService.softRemove(id);
   }
 }
